a6234012d41a5556e611efac19420ba1af70c66f
Mahmoud <mahmoudnasser1561@gmail.com>
2025-10-13 10:24:01 +0300

    used variables

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,11 @@
 # Create the primary VPC for workloads
 resource "aws_vpc" "main" {
-  cidr_block           = "192.168.0.0/16"
+  cidr_block           = var.vpc_cidr
   enable_dns_hostnames = true
   enable_dns_support   = true
 
   tags = {
-    Name        = "learning-terraform"
+    Name        = var.environment
     Environment = "Lab"
     Managed_By  = "Terraform"
   }
