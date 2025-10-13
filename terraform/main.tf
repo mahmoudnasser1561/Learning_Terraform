ebd14fe2928ba7936be135a5ccc38beb7bf21ae9
Mahmoud <mahmoudnasser1561@gmail.com>
2025-10-13 09:50:31 +0300

    replace VPC

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,11 @@
 # Create the primary VPC for workloads
 resource "aws_vpc" "main" {
-  cidr_block           = "10.0.0.0/16"
+  cidr_block           = "192.168.0.0/16"
   enable_dns_hostnames = true
   enable_dns_support   = true
 
   tags = {
-    Name        = "Terraform Course"
+    Name        = "learning-terraform"
     Environment = "Lab"
     Managed_By  = "Terraform"
   }
